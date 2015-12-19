52961d6f096baa76eb710e6ec2308cc842222c32
richaverma18 <richuverma22@gmail.com>
2015-12-19 21:46:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,86 +1,88 @@
 var express = require('express');
 var Type = require('type-of-is');
 var bodyParser = require('body-parser');
-var app     = express();
+var app = express();
 var fs = require('fs');
 var request = require("request");
 var cheerio = require('cheerio');
 
-
 app.use(bodyParser.urlencoded({ extended: true })); 
 
+// for deploying app in heroku
+var port = process.env.PORT || 8080;
 
-
+// get call to load the form:
 app.get('/', function(req, res) {
   displayForm(res);
 });
 
 var url;
 
-var nextPage = true;
-
-	app.post('/url', function(req, res) {
-	url= req.body.name;
+//var nextPage = true;
+// POST call to process the github url and to display response:  
+app.post('/url', function(req, res) {
+  // Make a request call to the Url passed in form.
+  url= req.body.name;
   request({
   uri: url,
 }, function(error, response, body) {
-  var $ = cheerio.load(body);
 
-  var Result = 'hi';
-    
-	var openIssues = 	$('a.btn-link.selected').text();
+	// Using cheerio to process the html page:  
+	var $ = cheerio.load(body);
+	//var Result = 'hi';
+	// Get number of open issues, using jquery and parse the string as integer:
+	var openIssues = $('a.btn-link.selected').text();
 	var numIssues = parseInt((openIssues.replace(/ /g,'')).replace(/\n/g,'').split("O")[0]);
 	var times=[];
 	var dates=[];
+	// Number of open issues that were opened in the last 24 hours
 	var issuesHours=0;
+	// Number of open issues that were opened more than 24 hours ago but less than 7 days ago
 	var issuesWeek=0;
+	// Number of open issues that were opened more than 7 days ago
 	var otherIssues =0;
+	// Get current date:
 	var date = new Date().getTime();
 	
-	
-		$('time').each(function(i,elem){
+	// Get all the Time tags on the page and get date of each issue:
+	$('time').each(function(i,elem){
       
-	  times[i] = $(this).attr('datetime');
-	 // if (date > new Date(times[i]))
-	 console.log(times[i]);
-	  var dateTime = times[i].split("T");
-	  var hours = dateTime[1].split(":");
-	  var parts = dateTime[0].split('-');
-  // new Date(year, month [, day [, hours[, minutes[, seconds[, ms]]]]])
-		dates[i] = new Date(parts[0], parts[1]-1, parts[2], hours[0], hours[1] , hours[2].split("Z")[0]);
-	  if ( date-dates[i].getTime() < 24*60*60*1000)
-	   {
+	  	times[i] = $(this).attr('datetime');
+	 	// if (date > new Date(times[i]))
+		// console.log(times[i]);
+		 var dateTime = times[i].split("T");
+		 var hours = dateTime[1].split(":");
+		 var parts = dateTime[0].split('-');
+		 // new Date(year, month [, day [, hours[, minutes[, seconds[, ms]]]]])
+		 dates[i] = new Date(parts[0], parts[1]-1, parts[2], hours[0], hours[1] , hours[2].split("Z")[0]);
+	  	
+	  	// check if issue is one day old or one week old:
+	  	if ( date-dates[i].getTime() < 24*60*60*1000)
+		{
 			//console.log(issuesHours);
 			issuesHours++;
-	   }
-	   else if (date - dates[i].getTime() < 7*24*60*60*1000){
-		//	console.log(date-dates[i].getTime()+ "   oissueweek");
+		}
+		else if (date - dates[i].getTime() < 7*24*60*60*1000){
+			//console.log(date-dates[i].getTime()+ "   oissueweek");
 			issuesWeek++;
-	   }else{
+		}else{
 			nextPage = false;
-	   }
-	   
-	 // console.log( issuesHours + "   " + issuesWeek);
-		});
-	}
-
-	otherIssues = numIssues - issuesHours - issuesWeek;
+	   	}
+	   	// console.log( issuesHours + "   " + issuesWeek);
+	});
+    }
+    // Number of open issues that were opened more than 7 days ago:
+    otherIssues = numIssues - issuesHours - issuesWeek;
+    // send the table in response		
+    res.send('<h1> Result </h1><table> <tr> <td> Total number of open issues : </td>  <td> '+ numIssues +'</td></tr> <tr> <td>Number of open issues that were opened in the last 24 hours : </td> <td> '+ issuesHours +'</td> </tr> <tr> <td>Number of open issues that were opened more than 24 hours ago but less than 7 days ago : </td> <td>'+issuesWeek+'</td></tr> <tr> <td>Number of open issues that were opened more than 7 days ago : </td> <td>'+ otherIssues +'</td></tr></table>	');
 	
-	res.send('<h1> Result </h1><table> <tr> <td> Total number of open issues : </td>  <td> '+ numIssues +'</td></tr> <tr> <td>Number of open issues that were opened in the last 24 hours : </td> <td> '+ issuesHours +'</td> </tr> <tr> <td>Number of open issues that were opened more than 24 hours ago but less than 7 days ago : </td> <td>'+issuesWeek+'</td></tr> <tr> <td>Number of open issues that were opened more than 7 days ago : </td> <td>'+ otherIssues +'</td></tr></table>	');
-  });
-  
-   
-
-
-
-
-
-
-app.listen(8080, function() {
-  console.log('Server running at http://127.0.0.1:8080/');
 });
 
+app.listen(port, function() {
+	console.log('Server running');
+});
 
+// to display form in html page
 function displayForm(res) {
     fs.readFile('form.html', function (err, data) {
         res.writeHead(200, {
